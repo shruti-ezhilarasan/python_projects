ccc1bdb2ef6b555ed623eaefcfda4472894a93ba
shruti-ezhilarasan <106711281+shruti-ezhilarasan@users.noreply.github.com>
2024-12-01 14:43:20 -0600

    troubleboardgame.py

--- troubleboardgame.py
+++ troubleboardgame.py
@@ -1,14 +1,3 @@
-# By submitting this assignment, I agree to the following:
-#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
-#   "I have not given or received any unauthorized aid on this assignment."
-# Names:        Deepna Kanjee
-#               Clara Berg
-#               Saachi Jain
-#               Shruti Ezhilarasan
-# Section:      570
-# Assignment:   Team FInal Project
-# Date:         23 November 2024
-
 import turtle
 import random
 
